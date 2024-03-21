984e9b9a5565c1a7ef86c143fec2aedcbe6ff4d6
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-03-21 13:26:42 +0200

    Added device platform specific values

--- FirstMauiApp/LayoutExample.xaml.cs
+++ FirstMauiApp/LayoutExample.xaml.cs
@@ -5,5 +5,8 @@ public partial class LayoutExample : ContentPage
 	public LayoutExample()
 	{
 		InitializeComponent();
+
+		VStackLayout.Padding = DeviceInfo.Platform == DevicePlatform.iOS 
+			? new Thickness(30, 10, 30, 10) : new Thickness(25);
 	}
 }

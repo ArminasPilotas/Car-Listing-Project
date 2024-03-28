905d9678d1939805332b70a1001ba02236bfd7da
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-03-28 10:37:48 +0200

    Added CarService with predefined data

---
+++ FirstMauiApp/Services/CarService.cs
@@ -0,0 +1,47 @@
+﻿using FirstMauiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMauiApp.Services
+{
+    public class CarService
+    {
+        public List<Car> GetCars()
+        {
+            return new List<Car>()
+            {
+                new Car
+                {
+                    Id = 1, Make = "Honda", Model = "Fit", Vin = "123"
+                },
+                new Car
+                {
+                    Id = 2, Make = "Toyota", Model = "Prado", Vin = "123"
+                },
+                new Car
+                {
+                    Id = 3, Make = "Honda", Model = "Civic", Vin = "123"
+                },
+                new Car
+                {
+                    Id = 4, Make = "Audi", Model = "A5", Vin = "123"
+                },
+                new Car
+                {
+                    Id = 5, Make = "BMW", Model = "M3", Vin = "123"
+                },
+                new Car
+                {
+                    Id = 6, Make = "Nissan", Model = "Note", Vin = "123"
+                },
+                new Car
+                {
+                    Id = 7, Make = "Ferrari", Model = "Spider", Vin = "123"
+                }
+            }
+        }
+    }
+}

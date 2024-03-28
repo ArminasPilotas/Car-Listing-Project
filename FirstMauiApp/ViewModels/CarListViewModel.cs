44bedc644bde7e0dc0f1767d35b1d3db26962c0a
Arminas Pilotas <Arminas.Pilotas@bdo.global>
2024-03-28 11:38:39 +0200

    Implemented CarListViewModel

---
+++ FirstMauiApp/ViewModels/CarListViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using FirstMauiApp.Models;
+using FirstMauiApp.Services;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace FirstMauiApp.ViewModels
+{
+    public partial class CarListViewModel : BaseViewModel
+    {
+        private readonly CarService carService;
+        public ObservableCollection<Car> Cars { get; private set; } = [];
+
+        public CarListViewModel(CarService carService)
+        {
+            Title = "Car List";
+            this.carService = carService;
+        }
+
+        [RelayCommand]
+        async Task GetCarListAsync()
+        {
+            if (IsLoading) return;
+
+            try
+            {
+                IsLoading = true;
+                if (Cars.Any()) Cars.Clear();
+
+                var cars = carService.GetCars();
+                foreach (var car in cars) Cars.Add(car);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get cars: {ex.Message}.");
+                await Shell.Current.DisplayAlert("Error", "Failed to retrieve list of cars.", "Ok");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
